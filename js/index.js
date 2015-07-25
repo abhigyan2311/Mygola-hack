04d4a0bd065c671507d58fb9074cd4498a0f970b
Abhigyan <abhigyan2311@gmail.com>
2015-07-25 18:31:30 +0530

    UI initialize

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+angular.module('MyApp', ['ngMaterial', 'ngMessages'])
+
+.controller('AppCtrl', function($scope) {
+
+});
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-/**
- * Created by abhigyan on 25/07/15.
- */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var spawn = require("child_process").spawn;
+var process = spawn('python',["search.py", "Pune"]);
+
+process.stdout.on('data', function (data , err){
+console.log(data);
+    console.log(err);
+});
+
+
